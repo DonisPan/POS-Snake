f86d8c9007a9736d05cb57bd8799c95b9dc7f94e
DonisPan <donisvasek@gmail.com>
2025-01-03 21:56:43 +0100

    change the snake rendering to ncurses

--- src/client.c
+++ src/client.c
@@ -1,4 +1,6 @@
 #include <arpa/inet.h>
+#include <ncurses.h>
+#include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +19,8 @@ char map[MAP_WIDTH * MAP_HEIGHT];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *render_game(void *args);
+void start_server();
+int connect_to_server();
 
 void *render_game(void *args) {
   int client_socket = *(int *)args;
@@ -24,23 +28,41 @@ void *render_game(void *args) {
     pthread_mutex_lock(&mutex);
     recv(client_socket, map, sizeof(map), 0);
 
-    printf("\033c");
+    // printf("\033c");
+    //  for (int y = 0; y < MAP_HEIGHT; ++y) {
+    //    for (int x = 0; x < MAP_WIDTH; ++x) {
+    //      printf("%c ", map[y * MAP_WIDTH + x]);
+    //    }
+    //    putchar('\n');
+    //  }
+    //  fflush(stdout);
+
+    clear();
     for (int y = 0; y < MAP_HEIGHT; ++y) {
       for (int x = 0; x < MAP_WIDTH; ++x) {
-        printf("%c ", map[y * MAP_WIDTH + x]);
+        mvaddch(y, x, map[y * MAP_WIDTH + x]);
       }
-      putchar('\n');
     }
-    fflush(stdout);
+    refresh();
+
     pthread_mutex_unlock(&mutex);
     usleep(SNAKE_SPEED);
   }
   return NULL;
 }
 
-int main(int argc, char *argv[]) {
+void start_server() {
+  pid_t pid = fork();
+  if (pid == 0) {
+    execl("./server", "./server", NULL);
+  } else {
+    usleep(1000000);
+  }
+}
+
+int connect_to_server() {
   struct sockaddr_in server_address;
-  char buffer[1];
+  // char buffer[1];
 
   int client_socket = socket(AF_INET, SOCK_STREAM, 0);
   if (client_socket == -1) {
@@ -54,19 +76,60 @@ int main(int argc, char *argv[]) {
 
   if (connect(client_socket, (struct sockaddr *)&server_address,
               sizeof(server_address)) == -1) {
+    close(client_socket);
     perror("Connection failed!");
-    return EXIT_FAILURE;
+    return -1;
+  }
+  return client_socket;
+}
+
+int main(int argc, char *argv[]) {
+  // struct sockaddr_in server_address;
+
+  // int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+  // if (client_socket == -1) {
+  //   perror("Socket creation failed!");
+  //   return EXIT_FAILURE;
+  // }
+
+  // server_address.sin_family = AF_INET;
+  // server_address.sin_port = htons(PORT);
+  // server_address.sin_addr.s_addr = INADDR_ANY;
+
+  // if (connect(client_socket, (struct sockaddr *)&server_address,
+  //             sizeof(server_address)) == -1) {
+  //   perror("Connection failed!");
+  //   return EXIT_FAILURE;
+  // }
+
+  initscr();
+  cbreak();
+  noecho();
+  curs_set(0);
+  keypad(stdscr, TRUE);
+
+  int client_socket = connect_to_server();
+
+  if (client_socket == -1) {
+    printw("Starting server...");
+    refresh();
+    start_server();
+    client_socket = connect_to_server();
   }
 
-  printf("Connected to the server\n");
+  printw("Connected to the server\n");
+  refresh();
+
   pthread_t render_thread;
   pthread_create(&render_thread, NULL, render_game, &client_socket);
 
   while (1) {
-    printf("Enter_direction (wasd): ");
+    char buffer[1];
+    printw("Enter_direction (wasd): ");
+    refresh();
 
-    buffer[0] = getchar();
-    getchar();
+    buffer[0] = getch();
+    // getchar();
 
     if (buffer[0] == 'q') {
       break;
@@ -75,9 +138,10 @@ int main(int argc, char *argv[]) {
     send(client_socket, buffer, 1, 0);
   }
 
-  close(client_socket);
-  // pthread_cancel(render_thread);
-  pthread_join(render_thread, NULL);
 
+  close(client_socket);
+  pthread_cancel(render_thread);
+  pthread_join(render_thread, NULL);
+  endwin();
   return EXIT_SUCCESS;
 }
