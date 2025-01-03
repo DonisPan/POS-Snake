b490c00cfb869c9992eb4518645d80729eb25eae
DonisPan <donisvasek@gmail.com>
2025-01-03 18:22:12 +0100

    allow multiple clients

--- src/server.c
+++ src/server.c
@@ -15,6 +15,8 @@
 #define SNAKE_MAX_LENGTH 256
 #define SNAKE_SPEED 500000
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 typedef struct {
   int x;
   int y;
@@ -23,8 +25,8 @@ typedef struct {
 typedef struct {
   Snake_bit parts[SNAKE_MAX_LENGTH];
   int length;
-  int deltaX;
-  int deltaY;
+  int dirX;
+  int dirY;
 } Snake;
 
 typedef struct {
@@ -33,30 +35,29 @@ typedef struct {
   bool chomped;
 } Snack;
 
-Snake snake;
-Snack snacks[MAX_SNACKS];
+typedef struct {
+  int id;
+  int client_socket;
+  Snake snake;
+} Client_data;
 
+Snack snacks[MAX_SNACKS];
 char map[MAP_WIDTH * MAP_HEIGHT];
 
-bool game_end = false;
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void spawn_snake();
+void spawn_snake(Snake *snake);
 void spawn_snacks();
-void game_rules();
-void generate_map();
+void generate_map(Snake *snake);
 void *handle_client(void *args);
 void *move_snake(void *args);
 
-void spawn_snake() {
-  snake.length = 1;
-  // snake.parts[0].x = 1 + rand() % (MAP_WIDTH - 2);
-  // snake.parts[0].y = 1 + rand() % (MAP_HEIGHT - 2);
-  snake.parts[0].x = 1;
-  snake.parts[0].y = 1;
-  snake.deltaX = 1;
-  snake.deltaY = 0;
+void spawn_snake(Snake *snake) {
+  snake->length = 1;
+  snake->parts[0].x = 1 + rand() % (MAP_WIDTH - 2);
+  snake->parts[0].y = 1 + rand() % (MAP_HEIGHT - 2);
+  // snake.parts[0].x = 1;
+  // snake.parts[0].y = 1;
+  snake->dirX = 1;
+  snake->dirY = 0;
 }
 
 void spawn_snacks() {
@@ -67,29 +68,7 @@ void spawn_snacks() {
   }
 }
 
-void game_rules() {
-  for (int i = 0; i < MAX_SNACKS; ++i) {
-    if (!snacks[i].chomped && snacks[i].x == snake.parts[0].x &&
-        snacks[i].y == snake.parts[0].y) {
-      snacks[i].chomped = true;
-      snake.length++;
-    }
-  }
-
-  if (snake.parts[0].x == 0 || snake.parts[0].x == MAP_WIDTH - 1 ||
-      snake.parts[0].y == 0 || snake.parts[0].y == MAP_HEIGHT - 1) {
-    game_end = true;
-  }
-
-  for (int i = 1; i < snake.length; ++i) {
-    if (snake.parts[0].x == snake.parts[i].x &&
-        snake.parts[0].y == snake.parts[i].y) {
-      game_end = true;
-    }
-  }
-}
-
-void generate_map() {
+void generate_map(Snake *snake) {
   for (int y = 0; y < MAP_HEIGHT; ++y) {
     for (int x = 0; x < MAP_WIDTH; ++x) {
       if (x == 0 || y == 0 || x == MAP_WIDTH - 1 || y == MAP_HEIGHT - 1) {
@@ -106,81 +85,100 @@ void generate_map() {
     }
   }
 
-  for (int i = 0; i < snake.length; ++i) {
-    map[snake.parts[i].y * MAP_WIDTH + snake.parts[i].x] = (i == 0) ? '0' : 'o';
+  for (int i = 0; i < snake->length; ++i) {
+    map[snake->parts[i].y * MAP_WIDTH + snake->parts[i].x] =
+        (i == 0) ? '0' : 'o';
   }
 }
 
 void *move_snake(void *args) {
-  int client_socket = *(int *)args;
+  Client_data *client_data = (Client_data *)args;
+  Snake *snake = &client_data->snake;
+  bool game_end = false;
 
   while (!game_end) {
     pthread_mutex_lock(&mutex);
-
-    for (int i = snake.length - 1; i > 0; --i) {
-      snake.parts[i] = snake.parts[i - 1];
+    for (int i = snake->length - 1; i > 0; --i) {
+      snake->parts[i] = snake->parts[i - 1];
     }
 
-    snake.parts[0].x += snake.deltaX;
-    snake.parts[0].y += snake.deltaY;
+    snake->parts[0].x += snake->dirX;
+    snake->parts[0].y += snake->dirY;
 
-    game_rules();
-    generate_map();
+    if (snake->parts[0].x <= 0 || snake->parts[0].x >= MAP_WIDTH - 1 ||
+        snake->parts[0].y <= 0 || snake->parts[0].y >= MAP_HEIGHT - 1) {
+      game_end = true;
+    }
 
-    send(client_socket, map, sizeof(map), 0);
+    for (int i = 1; i < snake->length; ++i) {
+      if (snake->parts[0].x == snake->parts[i].x &&
+          snake->parts[0].y == snake->parts[i].y) {
+        game_end = true;
+      }
+    }
+
+    for (int i = 0; i < MAX_SNACKS; ++i) {
+      if (!snacks[i].chomped && snacks[i].x == snake->parts[0].x &&
+          snacks[i].y == snake->parts[0].y) {
+        snacks[i].chomped = true;
+        snake->length++;
+      }
+    }
+
+    generate_map(snake);
+    send(client_data->client_socket, map, sizeof(map), 0);
+
     pthread_mutex_unlock(&mutex);
-
     usleep(SNAKE_SPEED);
   }
-  close(client_socket);
+
+  close(client_data->client_socket);
+  printf("Client %d disconnected.\n", client_data->id);
+  free(client_data);
   return NULL;
 }
 
 void *handle_client(void *args) {
-  int client_socket = *(int *)args;
+  Client_data *client_data = (Client_data *)args;
+  Snake *snake = &client_data->snake;
 
-  pthread_mutex_lock(&mutex);
-  printf("Sending initial map: \n");
-  for (int y = 0; y < MAP_HEIGHT; ++y) {
-    for (int x = 0; x < MAP_WIDTH; ++x) {
-      putchar(map[y * MAP_WIDTH + x]);
-    }
-    putchar('\n');
-  }
-  pthread_mutex_unlock(&mutex);
+  printf("Client %d connected.\n", client_data->id);
 
-  while (!game_end) {
+  pthread_t move_snake_thread;
+  pthread_create(&move_snake_thread, NULL, move_snake, client_data);
+
+  while (1) {
     char buffer[1];
-
-    recv(client_socket, buffer, 1, 0);
+    recv(client_data->client_socket, buffer, 1, 0);
 
     pthread_mutex_lock(&mutex);
-
     switch (buffer[0]) {
     case 'w':
-      snake.deltaX = 0;
-      snake.deltaY = -1;
+      snake->dirX = 0;
+      snake->dirY = -1;
       break;
     case 's':
-      snake.deltaX = 0;
-      snake.deltaY = 1;
+      snake->dirX = 0;
+      snake->dirY = 1;
       break;
     case 'a':
-      snake.deltaX = -1;
-      snake.deltaY = 0;
+      snake->dirX = -1;
+      snake->dirY = 0;
       break;
     case 'd':
-      snake.deltaX = 1;
-      snake.deltaY = 0;
+      snake->dirX = 1;
+      snake->dirY = 0;
       break;
     default:
       break;
     }
-
     pthread_mutex_unlock(&mutex);
+
+    usleep(SNAKE_SPEED);
   }
 
-  close(client_socket);
+  pthread_cancel(move_snake_thread);
+  pthread_join(move_snake_thread, NULL);
 
   return NULL;
 }
@@ -189,18 +187,13 @@ int main(int argc, char *argv[]) {
   srand(time(NULL));
   spawn_snacks();
 
-  generate_map();
-
-  int server_socket;
-
-  struct sockaddr_in server_address;
-
-  server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
   if (server_socket == -1) {
     perror("Socket creation failed!\n");
     return EXIT_FAILURE;
   }
 
+  struct sockaddr_in server_address;
   server_address.sin_family = AF_INET;
   server_address.sin_addr.s_addr = INADDR_ANY;
   server_address.sin_port = htons(PORT);
@@ -218,24 +211,26 @@ int main(int argc, char *argv[]) {
 
   printf("Server is listening on port: %d\n", PORT);
 
-  while (!game_end) {
+  int client_id = 0;
+  while (1) {
     struct sockaddr_in client_address;
     socklen_t client_length = sizeof(client_address);
 
     int client_socket = accept(
         server_socket, (struct sockaddr *)&client_address, &client_length);
     if (client_socket == -1) {
-      perror("Accept failed!");
-      return EXIT_FAILURE;
+      perror("Accept failure!\n");
+      continue;
     }
 
-    printf("Client connected\n");
-    spawn_snake();
-    pthread_t move_snakes;
-    pthread_create(&move_snakes, NULL, move_snake, &client_socket);
+    Client_data *client_data = malloc(sizeof(Client_data));
+    client_data->id = client_id;
+    ++client_id;
+    client_data->client_socket = client_socket;
+    spawn_snake(&client_data->snake);
 
     pthread_t client_thread;
-    pthread_create(&client_thread, NULL, handle_client, &client_socket);
+    pthread_create(&client_thread, NULL, handle_client, client_data);
     pthread_detach(client_thread);
   }
 
